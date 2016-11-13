31333c8c216ca7fe701bef0715bcc079417d4eda
Vallo <vallo61@gmail.com>
2016-11-13 18:40:45 -0300

    la desc va en el body del post

--- routes/alertaVial.js
+++ routes/alertaVial.js
@@ -4,7 +4,7 @@ var chofer = require('../model/chofer.js');
 var notifications = require('../helper/notifications.js');
 
 router.post('/', function(req,res){
-	var desc = req.get('desc');
+	var desc = req.body.desc;
 	chofer.findAll().then(function(chof){
 		for (var i = 0; i < chof.length; i++) {
 			if(chof[i].android_id){
