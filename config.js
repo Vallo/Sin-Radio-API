484ebe54521fb8de52689908a6ba491934d75362
Vallo <vallo61@gmail.com>
2017-04-10 12:46:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,4 @@ var config = {};
 config.radioNotif = {};
 config.radioNotif.cliente = 150;
 config.radioNotif.chofer = 3000;
-/*config.web = {};
-config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
-config.twitter.user_name = process.env.TWITTER_USER || 'username';
-config.twitter.password=  process.env.TWITTER_PASSWORD || 'password';
-config.redis.uri = process.env.DUOSTACK_DB_REDIS;
-config.redis.host = 'hostname';
-config.redis.port = 6379;
-config.web.port = process.env.WEB_PORT || 9980;
-*/
 module.exports = config;
